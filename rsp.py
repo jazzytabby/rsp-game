3b80e47034ddc42502dba6ad416ba98f433d6af0
Adelaide <mhsb.sifre@gmail.com>
2024-03-08 15:23:10 +0300

    First commit

---
+++ rsp.py
@@ -0,0 +1,40 @@
+from random import choice
+
+user_points = 0
+computer_points = 0
+moves = ["rock", "scissors", "paper"]
+move_values = {
+    "rock": 1,
+    "scissors": 2,
+    "paper": 3
+}
+
+while user_points < 3 and computer_points < 3:
+    user_move = input("Make your move: ").lower()
+
+    while user_move not in moves:
+        print("Invalid move. Valid moves are 'rock', 'scissors', 'paper'")
+        user_move = input("Make your move: ").lower()
+
+    computer_move = choice(moves)
+    user_value = move_values[user_move]
+    computer_value = move_values[computer_move]
+    calc = (user_value - computer_value) % 3
+
+    # User wins
+    if calc == 1:
+        user_points += 1
+        print(f"User: {user_points}\nComputer: {computer_points}")
+
+    elif calc == 2:
+        computer_points += 1
+        print(f"User: {user_points}\nComputer: {computer_points}")
+
+    else:
+        print("It's a tie!")
+        print(f"User: {user_points}\nComputer: {computer_points}")
+
+if user_points == 3:
+    print("You won!")
+else:
+    print("You lost :(")
